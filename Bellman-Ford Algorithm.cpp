3dbfd407960e8b626a5bac6baaa09c8465a70eee
aldoyaputrahartono <31863229+aldoyaputrahartono@users.noreply.github.com>
2020-04-29 21:37:12 +0700

    Update Bellman-Ford Algorithm.cpp

--- Bellman-Ford Algorithm.cpp
+++ Bellman-Ford Algorithm.cpp
@@ -4,35 +4,34 @@ using namespace std;
 
 typedef pair<int, int> iPair; 
   
-void addEdge(vector <pair<int, int> > adj[], int u, int v, int wt){ //adjacency list utk undirected
+void addEdge(vector <pair<int, int> > adj[], int u, int v, int wt){ //adjacency list utk directed
     adj[u].push_back(make_pair(v, wt)); 
 } 
   
 void BellmanFord(vector<pair<int,int> > adj[], int V, int src){ 
     vector<pair<int,int> > :: iterator x;		// iterator. Cara kerja nya mirip saat pakai variabel int i di loop, tetapi iterator memakai pointer
-	vector<int> dist(V, INF);  					// dist utk menampung jarak sementara suatu vertex dari source. Set awal infinite. INF ada di line 3
+    vector<int> dist(V, INF);  				// dist utk menampung jarak sementara suatu vertex dari source. Set awal infinite. INF ada di line 3
     dist[src] = 0; 
   
     for (int i = 0; i < V; i++) { 
     	for(x=adj[i].begin();x!=adj[i].end();++x){ 
-            int v = x->first; 					// v utk menampung destination
+            int v = x->first; 				// v utk menampung destination
             int weight = x->second; 
             // jika jarak dari source ke vertex-i tdk infinite dan jarak ke vertex-v lbh besar drpd (jarak awal + weight)
             if (dist[i] != INF && dist[i] + weight < dist[v]) dist[v] = dist[i] + weight;
-		}
+	}
     } 
   
-    // looping lagi seperti diatas utk mengecek apakah ada negative weight cycle
-    // yaitu cycle yang weight nya negatif semua
-	for (int i = 0; i < V; i++) { 
+    // looping lagi seperti diatas utk mengecek apakah ada negative weight cycle yaitu cycle yang weight nya negatif semua
+    for (int i = 0; i < V; i++) { 
     	for(x=adj[i].begin();x!=adj[i].end();++x){ 
             int v = x->first; 
             int weight = x->second; 
             if (dist[i] != INF && dist[i] + weight < dist[v]){
             	printf("Graph contains negative weight cycle"); 
             	return; // klo ketemu lngsng return 
-			}
-		}
+	    }
+	}
     } 
   
     printf("Vertex	Distance from Vertex %d\n",src); 
@@ -41,9 +40,9 @@ void BellmanFord(vector<pair<int,int> > adj[], int V, int src){
 } 
   
 int main(){ 
-	// Algoritmanya mirip dengan Dijkstra's, hanya saja weight disini dpt bernilai negatif
-    int V = 5;					// banyak vertex
-    vector<iPair > adj[V]; 		// pakai adjacency list. iPair disini pair<int, int> untuk pair<destination, weight>
+    // Algoritmanya mirip dengan Dijkstra's, hanya saja weight disini dpt bernilai negatif
+    int V = 5;			// banyak vertex
+    vector<iPair > adj[V]; 	// pakai adjacency list. iPair disini pair<int, int> untuk pair<destination, weight>
     addEdge(adj, 0, 1, -1); 	// input graph. Formatnya addEdge(adj, source, destination, weight)
     addEdge(adj, 0, 2, 4); 
     addEdge(adj, 1, 2, 3); 
