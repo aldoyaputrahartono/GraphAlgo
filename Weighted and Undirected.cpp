2d5ed0ab81f42792119baae6f527c85ffa93b3d6
aldoyaputrahartono <31863229+aldoyaputrahartono@users.noreply.github.com>
2020-04-29 19:18:08 +0700

    Update Weighted and Undirected.cpp

--- Weighted and Undirected.cpp
+++ Weighted and Undirected.cpp
@@ -7,12 +7,12 @@ void addEdge(vector<pair<int,int> > adj[], int u, int v, int wt){ //adjacency li
 } 
 
 void printGraph(vector<pair<int,int> > adj[], int V){ 
-	vector<pair<int,int> > :: iterator it;	// iterator. Cara kerja nya mirip saat pakai variabel int i di loop, tetapi iterator memakai pointer
+    vector<pair<int,int> > :: iterator it;	// iterator. Cara kerja nya mirip saat pakai variabel int i di loop, tetapi iterator memakai pointer
     int dst, weight; 
     for (int src = 0; src < V; src++){ 
         cout << "Node " << src << " makes an edge with \n"; 
         for (it = adj[src].begin(); it!=adj[src].end(); it++){ 
-            dst = it->first; 		// krn tadi pair<destination, weight> maka destination jd first dan weight jd second
+            dst = it->first; 			// krn tadi pair<destination, weight> maka destination jd first dan weight jd second
             weight = it->second; 
             cout << "\tNode " << dst << " with edge weight =" << weight << "\n"; 
         } 
@@ -21,16 +21,16 @@ void printGraph(vector<pair<int,int> > adj[], int V){
 } 
 
 int main(){ 
-    int V = 5; 							// banyak vertex
+    int V = 5; 				// banyak vertex
     vector<pair<int, int> > adj[V]; 	// adjacency list dmn isi pairnya <destination, weight>
-    addEdge(adj, 0, 1, 10); 			// input graphnya. Format addEdge(adj, source, destination, weight)
+    addEdge(adj, 0, 1, 10); 		// input graphnya. Format addEdge(adj, source, destination, weight)
     addEdge(adj, 0, 4, 20); 
     addEdge(adj, 1, 2, 30); 
     addEdge(adj, 1, 3, 40); 
     addEdge(adj, 1, 4, 50); 
     addEdge(adj, 2, 3, 60); 
     addEdge(adj, 3, 4, 70); 
-    printGraph(adj, V); 				// print graphnya
+    printGraph(adj, V); 		// print graphnya
     // klo graphnya sdh weighted maka BFS DFS jd gk seberapa berguna krn lbh berfokus ke weightnya
     // ntar problem yang muncul biasanya MST sama shortest path
     return 0; 
