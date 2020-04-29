09eb4bbe66f84897d633844c78ef7df9b69b9ed5
aldoyaputrahartono <31863229+aldoyaputrahartono@users.noreply.github.com>
2020-04-29 18:39:15 +0700

    Update Unweighted and Undirected.cpp

--- Unweighted and Undirected.cpp
+++ Unweighted and Undirected.cpp
@@ -7,18 +7,18 @@ void addEdge(vector<int> adj[], int u, int v){ //adjacency list utk undirected
 }
 
 void BFS(vector<int> adj[], int V){ 
-    bool *visited = new bool[V]; 						// utk ngecek sudah pernah dilewati atau blm
+    bool *visited = new bool[V]; 				// utk ngecek sudah pernah dilewati atau blm
     for(int i = 0; i < V; i++) visited[i] = false; 		// semua di set false (blm prnh dilewati)
     vector<int> ::iterator it; 		// iterator. Cara kerja nya mirip saat pakai variabel int i di loop, tetapi iterator memakai pointer
-    queue<int> queue; 				// deklarasi queue
-    visited[0] = true; 				// visited[source]=true		disini aku anggap vertex 0 sebagai source
-    queue.push(0); 					// push source ke queue
+    queue<int> queue; 			// deklarasi queue
+    visited[0] = true; 			// visited[source]=true		disini aku anggap vertex 0 sebagai source
+    queue.push(0); 			// push source ke queue
     while(!queue.empty()){
         int s = queue.front(); 
-        cout << s << " "; 			// cetak deh
+        cout << s << " "; 		// cetak deh
         queue.pop(); 
         for (it = adj[s].begin(); it != adj[s].end(); ++it){ 
-            if (!visited[*it]){ 		// klo blm visited maka visited dan push ke queue
+            if (!visited[*it]){ 	// klo blm visited maka visited dan push ke queue
                 visited[*it] = true; 
                 queue.push(*it); 
             } 
@@ -41,7 +41,7 @@ void DFS(vector<int> adj[], int V){
             DFSUtil(u, adj, visited); 		// panggil DFS rekursif
 } 
 
-void DFS_iteratif(vector<int> adj[], int V){ 			//cara kerjanya sama kyk BFS, hanya saja memakai stack
+void DFS_iteratif(vector<int> adj[], int V){ 	//cara kerjanya sama kyk BFS, hanya saja memakai stack
     bool *visited = new bool[V]; 
     for(int i = 0; i < V; i++) visited[i] = false; 
     vector<int> ::iterator it; 
@@ -62,7 +62,7 @@ void DFS_iteratif(vector<int> adj[], int V){ 			//cara kerjanya sama kyk BFS, ha
 } 
 
 int main(){ 
-    int V = 9;				// input banyak vertex
+    int V = 9;			// input banyak vertex
     vector<int> adj[V];		// aku pakai adjacency list
     addEdge(adj, 0, 1); 	// input graph nya. Format addEdge(adj, source, destination)
     addEdge(adj, 0, 7); 
@@ -83,6 +83,6 @@ int main(){
     cout<<"\n\nDFS Stack:\n";
     DFS_iteratif(adj, V);
     cout<<"\n\nDFS Recursive:\n";	// sebisa mungkin hindari rekursif krn makan bnyk memory
-	DFS(adj, V); 
+    DFS(adj, V); 
     return 0; 
 } 
